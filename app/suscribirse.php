e50bb7bff4f1a8b711a69796bea462ee7ea7a846
Simei Smoler <38670971+simeismoler@users.noreply.github.com>
2023-05-25 05:05:46 -0300

    .

--- app/suscribirse.php
+++ app/suscribirse.php
@@ -26,7 +26,7 @@
 
 	<!-- Document Title
 	============================================= -->
-	<title>Sonhando Alto - Inscreva-se</title>
+	<title>Sueña en Grande - Suscribirse</title>
 
 </head>
 
@@ -45,8 +45,8 @@
 			<div class="gmap min-vh-60 min-vh-lg-100" style="background-image: url('demos/restaurant/images/vem.jpg'); background-size: cover"></div>
 
 			<div class="p-5 reservation-form rounded bg-white">
-				<h3 class="font-secondary h1 color">Inscreva-se.</h3>
-				<div>O preenchimento de todos os campos é obrigatório.</div>
+				<h3 class="font-secondary h1 color">Suscribirse</h3>
+				<div>Completar todos los campos es obligatorio.</div>
 
 				<div class="form-widget mt-4 mt-lg-0" data-loader="button">
 
@@ -61,58 +61,33 @@
 						</div>
 
 						<div class="col-sm-6 mb-3">
-							<input type="text" id="template-contactform-name" name="template-contactform-name" value="" class="sm-form-control border-form-control required" placeholder="Nome" />
+							<input type="text" id="template-contactform-name" name="template-contactform-name" value="" class="sm-form-control border-form-control required" placeholder="Nombre" />
 						</div>
                         <div class="col-sm-6 mb-3">
-							<input type="text" id="template-contactform-idade" name="template-contactform-idade" value="" class="sm-form-control border-form-control required" placeholder="Idade" />
+							<input type="text" id="template-contactform-idade" name="template-contactform-idade" value="" class="sm-form-control border-form-control required" placeholder="Edad" />
 						</div>
 						<div class="col-sm-6 mb-3">
 							<input type="email" id="template-contactform-email" name="template-contactform-email" value="" class="required email sm-form-control border-form-control" placeholder="E-mail" />
 						</div>
                         <div class="col-sm-6 mb-3">
-							<input type="text" id="template-contactform-celular" name="template-contactform-celular" value="" class="required sm-form-control border-form-control" placeholder="Celular/WhatsApp" />
+							<input type="text" id="template-contactform-celular" name="template-contactform-celular" value="" class="required sm-form-control border-form-control" placeholder="Teléfono móvil/WhatsApp" />
 						</div>
                         <div class="col-sm-6 mb-3">
 							<input type="text" id="template-contactform-instagram" name="template-contactform-instagraml" value="" class="required sm-form-control border-form-control" placeholder="Instagram" />
 						</div>
                         <div class="col-sm-6 mb-3">
-							<input type="text" id="template-contactform-cidade" name="template-contactform-cidade" value="" class="required sm-form-control border-form-control" placeholder="Cidade" />
+							<input type="text" id="template-contactform-cidade" name="template-contactform-cidade" value="" class="required sm-form-control border-form-control" placeholder="Ciudad" />
 						</div>
                         <div class="col-sm-6 mb-3">
-							<input type="text" id="template-contactform-cep" name="template-contactform-cep" value="" class="required sm-form-control border-form-control" placeholder="CEP" />
+							<input type="text" id="template-contactform-estado" name="template-contactform-estado" value="" class="required sm-form-control border-form-control" placeholder="Estado" />
 						</div>
                         <div class="col-6 mb-3">
 							<select class="custom-select sm-form-control border-form-control"  id="template-contactform-estado" name="template-contactform-estado">
-								<option value="disabled" value="" disabled selected>Estado</option>
-                                <option value="AC">Acre</option>
-                                <option value="AL">Alagoas</option>
-                                <option value="AP">Amapá</option>
-                                <option value="AM">Amazonas</option>
-                                <option value="BA">Bahia</option>
-                                <option value="CE">Ceará</option>
-                                <option value="DF">Distrito Federal</option>
-                                <option value="ES">Espírito Santo</option>
-                                <option value="GO">Goiás</option>
-                                <option value="MA">Maranhão</option>
-                                <option value="MS">Mato Grosso do Sul</option>
-                                <option value="MT">Mato Grosso</option>
-                                <option value="MG">Minas Gerais</option>
-                                <option value="PA">Pará</option>
-                                <option value="PB">Paraíba</option>
-                                <option value="PR">Paraná</option>
-                                <option value="PE">Pernambuco</option>
-                                <option value="PI">Piauí</option>
-                                <option value="RJ">Rio de Janeiro</option>
-                                <option value="RN">Rio Grande do Norte</option>
-                                <option value="RS">Rio Grande do Sul</option>
-                                <option value="RO">Rondônia</option>
-                                <option value="RR">Roraima</option>
-                                <option value="SC">Santa Catarina</option>
-                                <option value="SP">São Paulo</option>
-                                <option value="SE">Sergipe</option>
-                                <option value="TO">Tocantins</option>
-                               
-
+								<option value="disabled" value="" disabled selected>País</option>
+                                <option value="AR">Argentina</option>
+                                <option value="PE">Peru</option>
+                                <option value="BO">Bolivia</option>
+                                <option value="EC">Ecuador</option>
 							</select>
 						</div>
 
@@ -120,29 +95,29 @@
 
 						<div class="col-6 mb-3">
 							<select class="custom-select sm-form-control border-form-control"  id="template-contactform-nivel" name="template-contactform-nivel">
-                            <option value="disabled" disabled selected>Nível de Escolaridade</option>
-								<option value="Não iniciei o Ensino Médio">Não iniciei o Ensino Médio</option>
-								<option value="1º Ano">1º Ano</option>
-                                <option value="2º Ano">2º Ano</option>
-                                <option value="3º Ano">3º Ano</option>
-                                <option value="Já terminei o Ensino Médio">Já terminei o Ensino Médio</option>
-                                <option value="Já iniciei a faculdade">Já iniciei a faculdade</option>
+                            <option value="disabled" disabled selected>Nivel de escolaridad</option>
+								<option value="No comencé la secundaria">No comencé la secundaria</option>
+								<option value="Primero año">Primero año</option>
+                                <option value="Segundo año">Segundo año</option>
+                                <option value="Tercero año">Tercero año</option>
+                                <option value="Terminé la escuela secundaria">Terminé la escuela secundaria</option>
+                                <option value="Ya comencé la universidad">Ya comencé la universidad</option>
 
 							</select>
 						</div>
                         <div class="clear"></div>
                         <div class="col-sm-6 mb-3">
-							<input type="text" id="template-contactform-curso" name="template-contactform-curso" value="" class="required sm-form-control border-form-control" placeholder="Qual curso deseja fazer?" />
+							<input type="text" id="template-contactform-curso" name="template-contactform-curso" value="" class="required sm-form-control border-form-control" placeholder="¿Qué curso quieres tomar?" />
 						</div>
                         <div class="col-sm-6 mb-3">
-							<input type="text" id="template-contactform-universidade" name="template-contactform-universidade" value="" class="required sm-form-control border-form-control" placeholder="Em qual universidade deseja estudar?" />
+							<input type="text" id="template-contactform-universidade" name="template-contactform-universidade" value="" class="required sm-form-control border-form-control" placeholder="¿En qué universidad quieres estudiar?" />
 						</div>
 					
 
 						<div class="clear"></div>
 
 						<div class="col-12 mb-0">
-							<button class="button button-circle button-large text-white ms-0 mt-3" type="submit" id="template-contactform-submit" name="template-contactform-submit" value="submit">Enviar Formulário</button>
+							<button class="button button-circle button-large text-white ms-0 mt-3" type="submit" id="template-contactform-submit" name="template-contactform-submit" value="submit">Enviar Formulario</button>
 						</div>
 
 						<div class="clear"></div>
