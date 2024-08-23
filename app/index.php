05c0d00b9514a5c66b2d7032e860d73f9bb2326e
Simei Smoler <38670971+simeismoler@users.noreply.github.com>
2024-08-23 18:11:33 -0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -107,8 +107,8 @@
 								<h3>&nbsp;</h3>
 								<iframe width="560" height="315" src="https://www.youtube.com/embed/3l2XM_bPYbA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 							</div>
-						</div>
-
+						</div> 
+ 
 
 					</div>
 				</div>
