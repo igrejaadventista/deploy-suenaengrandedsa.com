89abdad1e4a89184aaa3b599f79043b841baee67
Simei Smoler <38670971+simeismoler@users.noreply.github.com>
2024-08-23 14:58:58 -0300

    Update meta.php

--- app/meta.php
+++ app/meta.php
@@ -3,9 +3,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Sonhando Alto</title>
 	<!-- Código para Google Authorship e Publisher -->
-	<meta name="author" content="Sonhando Alto">
-	<!-- Código do Schema.org também para o Google+ -->
-	<meta itemprop="name" content="Meu sonho possível">
 	<meta itemprop="description"
 		content="Conheça mais do Programa Sonhando ALTO, que já mudou a vida de milhares de jovens como você. O futuro se faz no presente, se faz a cada dia e se faz com escolhas e atitudes.">
 	<meta itemprop="image" content="./img/banner_social.jpg">
@@ -20,13 +17,12 @@
 	<meta name="twitter:image" content="./img/banner_social.jpg">
 	<meta property="og:title" content="Meu sonho possível">
 	<meta property="og:type" content="website">
-	<meta property="og:url" content="https://sonhandoaltobrasil.com">
+	<meta property="og:url" content="https://suenaengrandedsa.com/">
 	<meta property="og:image" content="./img/banner_social.jpg">
 	<meta property="og:image:type" content="image/jpeg">
 	<meta property="og:image:width" content="1024">
 	<meta property="og:image:height" content="540">
 	<meta property="article:section" content="entretenimento">
-	<meta property="article:author" content="https://www.facebook.com/adventistasbrasiloficial">
 	<meta property="og:description"
 		contenDt="Conheça mais do Programa Sonhando ALTO, que já mudou a vida de milhares de jovens como você. O futuro se faz no presente, se faz a cada dia e se faz com escolhas e atitudes.">
 	<meta property="og:site_name" content="Meu sonho possível">
